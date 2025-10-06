6a7f3b1b20336bd50fa07ce175dffe680bdc4409
Umang Saini <sainiumang505@gmail.com>
2025-10-06 21:48:03 +0530

    errors

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import app from "./app.js";
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
 
-
 const port = process.env.PORT || 3000;
 
 dotenv.config({
     path: "./.env",
-});// Load environment variables from .env file
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
