44e181e35ebca14d89ffe3302bfd1f5db02ccd76
Umang Saini <sainiumang505@gmail.com>
2025-10-09 23:28:01 +0530

    bun

--- Back2.0/serve-bun.js
+++ Back2.0/serve-bun.js
@@ -5,6 +5,13 @@ serve({
         const url = new URL(request.url);
         if (url.pathname === '/') {
             return new Response('Hello', { status: 200 })
+        } else if (url.pathname === '/user') {
+            return new Response('Hello user', { status: 200 })
         }
-    }
+        else {
+            return new Response()({ status: 404 })
+        }
+    },
+    port: 3000,
+    hostname: '127.0.0.1'
 })
