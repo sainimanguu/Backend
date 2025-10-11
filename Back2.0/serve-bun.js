237bd2734be91344f8fc9f9214750d2db089a4b4
Umang Saini <sainiumang505@gmail.com>
2025-10-11 21:21:22 +0530

    files

--- Back2.0/serve-bun.js
+++ Back2.0/serve-bun.js
@@ -12,6 +12,17 @@ serve({
             return new Response()({ status: 404 })
         }
     },
+    fetch(request) {
+        const url = new URL(request.url);
+        if (url.pathname === '/') {
+            return new Response('Hello', { status: 200 })
+        } else if (url.pathname === '/user') {
+            return new Response('Hello user', { status: 200 })
+        }
+        else {
+            return new Response()({ status: 404 })
+        }
+    },
     port: 3000,
     hostname: '127.0.0.1'
 })//server-made by bun 
